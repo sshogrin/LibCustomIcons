71ac045900c7494e7aca6a8a9bf95d94238022d4
m00nyONE <severin.meier.x@gmail.com>
2025-06-29 15:31:10 +0200

    update icons

---
+++ icons/animated.lua
@@ -0,0 +1,8 @@
+local lib_name = "LibCustomIcons"
+local lib = _G[lib_name]
+local a = lib.GetAnimatedTable()
+
+a["pikadab"] = {"LibCustomIcons/icons/animated/pikadab.dds", 7, 7, 20}
+a["pikahi"] = {"LibCustomIcons/icons/animated/pikahi.dds", 69, 1, 33}
+a["catjam"] = {"LibCustomIcons/icons/animated/catjam.dds", 79, 2, 25}
+a["dancingcat"] = {"LibCustomIcons/icons/animated/dancingcat.dds", 6, 6, 20}

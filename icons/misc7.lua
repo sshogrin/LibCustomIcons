856de86631109a1006f1d168cbdf6492cdf8586a
m00nyONE <severin.meier.x@gmail.com>
2025-07-25 23:09:40 +0200

    add/edit/remove users

--- icons/misc7.lua
+++ icons/misc7.lua
@@ -195,3 +195,4 @@ a["@ewilliam"] = {"LibCustomIcons/icons/misc7/ewilliam_anim.dds", 19, 1, 20}
 s["@Chay'Kashet"] = "LibCustomIcons/icons/misc7/ChayKashet.dds"
 s["@Pocket3591"] = "LibCustomIcons/icons/misc7/Pocket3591.dds"
 a["@Pocket3591"] = {"LibCustomIcons/icons/misc7/Pocket3591_anim.dds", 5, 1, 10}
+s["@InsaneShad0w92"] = "LibCustomIcons/icons/misc7/InsaneShad0w92.dds"
